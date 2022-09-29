5ea0b721da6188082d9b782dc6668f0dcf92dfe4
Saish Rane <saishrane06@gmail.com>
2022-09-29 18:18:11 +0530

    appy.py and web application page added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Airlines_Customer_Satisfaction_Model
+### Airlines_Customer_Satisfaction_Model
+
+### Software and Tools requirements
+1. [Github Account](https://github.com)
+2. [VS Code IDE](https://code.visualstudio.com/)
+3. [Git CLI](https://git-scm.com/book/env/v2/Getting-Started-The-Command-Line)
+4. [Heroku Account](https://heroku.com/)
+
+Create a new Environment for the project
+```
+conda create -p venv python==3.7 -y
+```
---
+++ app.py
@@ -0,0 +1,41 @@
+import json
+import pickle
+from flask import Flask,request,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+# Creating new app
+
+app=Flask(__name__)           #__name__ is nothing but the starting point of the app from where it will start
+regmodel=pickle.load(open('regmodel.pkl','rb'))   #Opening and then loading Pickle file for deployment
+scalar=pickle.load(open('scaling.pkl','rb'))
+
+##after localhost address if we put this app.route slash then it will redirect us to home.html page
+@app.route('/')     
+def home():
+    return render_template('home.html')
+
+# Creating predict api to fetch values using Postman or nay other tool
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(data))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1, -1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(float(output[0]))
+
+#
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    if output != 0.0:
+        return render_template("home.html",prediction_text="The Customer is not satisfied with the airline service")
+    return render_template("home.html",prediction_text="The Customer is satisfied with the airline service")
+
+if __name__=="__main__":
+    app.run(debug=True)
